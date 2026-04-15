a7c5495020e0777c9980db5fdd2814c5a1c2d532
Shaik sadiya sheeraj <123887154+sadiya2005@users.noreply.github.com>
2026-04-16 01:40:45 +0530

    hardcore

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,6 +2,6 @@
 // During development: http://127.0.0.1:8000
 // During production: Your Render URL (e.g. https://your-app.onrender.com)
 
-const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+const API_URL = "https://medical-backend-igni.onrender.com";
 
 export default API_URL;
